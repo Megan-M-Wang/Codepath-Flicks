1dc92da04b187d6a5febe79b29e07bb2cfbef228
Megan <Megan@MacBook-Pro-191.local>
2017-01-30 14:59:01 -0800

    Second Part

--- Flick_MovieViewer/Flick_MovieViewer/MovieViewController.swift
+++ Flick_MovieViewer/Flick_MovieViewer/MovieViewController.swift
@@ -15,38 +15,48 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     @IBOutlet weak var tableView: UITableView!
     
     var movies: [NSDictionary]?
+    var refreshControl = UIRefreshControl()
+    var endpoint: String = ""
     
+    
+    func netRequest(ifLoad: Bool) {
+        
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = URL(string: "https://api.themoviedb.org/3/movie/\(endpoint)?api_key=\(apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        if( ifLoad ) {
+            MBProgressHUD.showAdded(to: self.view, animated: true)
+        }
+        
+        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let data = data {
+                
+                if( ifLoad ) {
+                    MBProgressHUD.hide(for: self.view, animated: true)
+                }
+                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+                    
+                    self.movies = dataDictionary["results"] as? [NSDictionary]
+                    self.tableView.reloadData()
+                    self.refreshControl.endRefreshing()
+                    
+                }
+            }
+        }
+        task.resume()
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.dataSource = self
         tableView.delegate = self
         
-        let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(refreshControl:)), for: UIControlEvents.valueChanged)
         tableView.insertSubview(refreshControl, at: 0)
-        
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-        
-        MBProgressHUD.showAdded(to: self.view, animated: true)
-        
-        let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-            if let data = data {
-                
-                MBProgressHUD.hide(for: self.view, animated: true)
-                if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-
-                    self.movies = dataDictionary["results"] as? [NSDictionary]
-                    self.tableView.reloadData()
-                   
-                }
-            }
-        }
-        task.resume()
-        
+        netRequest(ifLoad: true)
     }
 
     override func didReceiveMemoryWarning() {
@@ -65,62 +75,43 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
     }
     
     public func refreshControlAction(refreshControl: UIRefreshControl) {
-        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
-        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-        
-        let session = URLSession(
-            configuration: URLSessionConfiguration.default,
-            delegate:nil,
-            delegateQueue:OperationQueue.main
-        )
-        
-        
-        let task : URLSessionDataTask = session.dataTask(with: request,
-                completionHandler: { (data, response, error) in
-                if let data = data {
-                    if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
-                        
-                        self.movies = dataDictionary["results"] as? [NSDictionary]
-                        self.tableView.reloadData()
-                    }
-                        
-                }
-                
-                
-                refreshControl.endRefreshing()
-        });
-        
-        task.resume()
-        
+        netRequest(ifLoad: false)
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        
+      
+        cell.selectionStyle = .blue
+
         let movie = movies![indexPath.row]
         let title = movie["title"] as! String
         let overview = movie["overview"] as! String
         let baseURL = "https://image.tmdb.org/t/p/w500"
-        let posterPath = movie["poster_path"] as! String
-        let imageURL = NSURL(string: baseURL + posterPath)
+        if let posterPath = movie["poster_path"] as? String {
+            let imageURL = NSURL(string: baseURL + posterPath)
         
-        cell.posterView.setImageWith(imageURL as! URL)
+            cell.posterView.setImageWith(imageURL as! URL)
+        }
         
         cell.titleLabel.text = title
         cell.overviewLabel.text = overview
+        cell.selectionStyle = .none
         
         return cell
     }
     
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)
+        let movie = movies![indexPath!.row]
+        let detailViewController = segue.destination as! DetailViewController
+        detailViewController.movie = movie
     }
-    */
-
 }
